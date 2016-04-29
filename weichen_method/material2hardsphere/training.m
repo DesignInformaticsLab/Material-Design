18ec9c206887972f3b292ab4a63106e1a9228a36
p2admin <renyi.tsinghua@gmail.com>
2016-04-28 21:30:50 -0700

    still working on Weichen's method

---
+++ weichen_method/material2hardsphere/training.m
@@ -0,0 +1,53 @@
+%%%% training proposed models
+% all training data are stored under ./training_data
+
+
+%% train channel-wise conditional probability models
+% data: channel_conditional
+% - X, y are pixels sampled from each channel
+% - X (batch_size*nimage*nchannel,patch_size^2-2+nchannel)
+% - y (batch_size*nimage*nchannel,1)
+% - batch_list contains pixel indices from each channel and image
+
+% load data
+patch_size = 25;
+batch_size = 100;
+file = ['./training_data/channel_conditional_patch',num2str(patch_size),...
+    '_batch',num2str(batch_size),'_0426.mat'];
+load(file);
+
+% try logistic regression
+
+% add liblinear
+addpath('../libsvm');   
+
+nchannel = 1; % 2nd layer filter size
+nimage = 60; %100 images
+H = 200; % image height
+W = 200; % image width
+margin_size = (patch_size-1)/2;
+
+% train channel-wise models
+models = cell(nchannel,1);
+cset = [1e-6, 1e-4, 1e-2, 1];
+cverror = zeros(length(cset),1);
+for i = 1:nchannel
+    yy = y((i-1)*(nimage*batch_size)+(1:batch_size*nimage),:);
+    XX = X((i-1)*(nimage*batch_size)+(1:batch_size*nimage),:);
+    for cid = 1:length(cset)
+        cverror(cid) = svmtrain(sparse(yy), sparse(XX), sprintf(['-v 3 -s 0 -e 1e-6 -b 1 -q -c ', num2str(cset(cid))]));
+    end
+    models{i,1} = svmtrain(sparse(yy), sparse(XX), sprintf(['-s 0 -e 1e-6 -b 1 -q -c ',num2str(cset(find(cverror==max(cverror),1)))]));
+end
+
+
+% test training error
+for i = 1:nchannel
+    yy = y((i-1)*(nimage*batch_size)+(1:batch_size*nimage),:);
+    XX = X((i-1)*(nimage*batch_size)+(1:batch_size*nimage),:);
+    [predicted_label, accuracy] = svmpredict(sparse(yy),sparse(XX),models{i});
+end
+
+save(['./training_data/channel_conditional_model',num2str(patch_size),...
+    '_batch',num2str(batch_size),'_0426.mat'],...
+    'models','-v7.3');
---
+++ weichen_method/material3sandstone/generate_visible.m
@@ -0,0 +1,70 @@
+%%%% generate new 2nd layer hidden states
+addpath('../libsvm/');
+addpath('../');
+
+%% use gibbs sampling based on the conditional probabilities
+num_randomimage = 60;
+nchannel = 1; % 2nd layer filter size
+nimage = 60; %100 images
+H = 200; % image height
+W = 200; % image width
+patch_size = 45;
+margin_size = (patch_size-1)/2;
+batch_size = 100; % from each channel of each image, sample $batch_size$ patches
+
+load(['./training_data/channel_conditional_model',num2str(patch_size),...
+    '_batch',num2str(batch_size),'_0426.mat']);
+
+
+gibbs_iter = 1;
+max_gibbs_iter = 100;
+error_tol = 0.05;
+error = 1;
+
+% input original image pixel id, output pixel id for the image with margins
+image2margin = (1:H*W)'+...
+    kron((H*margin_size+((2*margin_size)+(1:2:(2*W-1)))'*margin_size),ones(H,1));
+
+patch_mesh_h = repmat((-margin_size:1:margin_size)',patch_size*H*W,1)+...
+    repmat(kron((1:H)'+margin_size,ones(patch_size^2,1)),W,1);
+patch_mesh_w = repmat(kron((-margin_size:1:margin_size)',ones(patch_size,1)),H*W,1)+...
+    kron((1:W)'+margin_size,ones(patch_size^2*H,1));
+
+all_pic = zeros(num_randomimage, H*W*nchannel);
+for idx = 1:num_randomimage
+    hidstate = ones(H,W,nchannel);
+    for c = 1:nchannel
+        fprintf('%%%%%%%%%%%%%%%%%%%% channel %%%%%%%%%%%%%%%%%%%%%');
+        pic = rand(H+2*margin_size,W+2*margin_size);
+        pic((1:H)+margin_size,(1:W)+margin_size) = hidstate(:,:,c);
+        
+        count = 1;
+        for w = 1:W
+            for h = 1:H
+                patch_sampled_h = patch_mesh_h((count-1)*patch_size^2+(1:patch_size^2));
+                patch_sampled_w = patch_mesh_w((count-1)*patch_size^2+(1:patch_size^2));
+                count = count + 1;
+
+                id = sub2ind(size(pic),patch_sampled_h,patch_sampled_w);
+                hx = pic(id);
+                hx((patch_size^2+1)/2:end) = [];
+                
+                vx = reshape(hidstate(h,w,[1:c-1,c+1:end]),nchannel-1,1);
+                
+                x = [hx;vx];
+                
+                [predicted_label, accuracy, prob] = ...
+                    svmpredict(sparse(hidstate(h,w,c)),sparse(x'),models{c}, sprintf('-b 1'));
+                id = models{c}.Label==1; % for white
+                if rand < prob(id)
+                    hidstate(h,w,c) = 1;
+                else
+                    hidstate(h,w,c) = -1;
+                end
+                pic((1:H)+margin_size,(1:W)+margin_size) = hidstate(:,:,c);
+            end
+        end
+    end
+    all_pic(idx,:) = hidstate(:);
+end
+save(['all_pic_material1alloy_patch_size',num2str(patch_size),'.mat'],'all_pic','-v7.3');
